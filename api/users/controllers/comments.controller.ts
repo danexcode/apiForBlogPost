778aa3cfc50a44721652bb76c10640bfe8da81dc
Danifanton <danielrxd0912@gmail.com>
2023-11-13 13:32:28 -0400

    Comment's controller completed

--- api/users/controllers/comments.controller.ts
+++ api/users/controllers/comments.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CommentsService } from '../services/comments.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateCommentDto, UpdateCommentDto } from '../dtos/comment.dto';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private commentsService: CommentsService) {}
+
+  @ApiOperation({ summary: 'Get comment by id' })
+  @Get('/:id')
+  get(@Param('id', ParseIntPipe) id: number) {
+    return this.commentsService.findOne(id);
+  }
+
+  @ApiOperation({ summary: 'Create comment' })
+  @Post()
+  create(@Body() payload: CreateCommentDto) {
+    return this.commentsService.create(payload);
+  }
+
+  @ApiOperation({ summary: 'Update comment by id' })
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateCommentDto,
+  ) {
+    return this.commentsService.update(id, payload);
+  }
+
+  @ApiOperation({ summary: 'Delete comment by id' })
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.commentsService.remove(id);
+  }
+}
