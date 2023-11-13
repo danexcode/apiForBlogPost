f23fec3fb3c34b6ad832e5d0c720e14bf96137a7
Danifanton <danielrxd0912@gmail.com>
2023-11-13 12:31:39 -0400

    User's service completed

--- api/users/services/users.service.ts
+++ api/users/services/users.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
+import { hash } from 'bcryptjs';
+import { Post } from 'api/posts/entities/post.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+    @InjectRepository(User)
+    private postRepo: Repository<Post>,
+  ) {}
+
+  findAll() {
+    return this.userRepo.find({
+      relations: ['customer'],
+    });
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepo.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
+
+  async create(data: CreateUserDto) {
+    const newUser = this.userRepo.create(data);
+    const hashPassword = await hash(newUser.password, 10);
+    newUser.password = hashPassword;
+    return this.userRepo.save(newUser);
+  }
+
+  async update(id: number, changes: UpdateUserDto) {
+    const user = await this.findOne(id);
+    this.userRepo.merge(user, changes);
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    const res = await this.userRepo.delete(id);
+    return res;
+  }
+
+  async getPostsByUser(id: number) {
+    const user = this.findOne(id);
+    const posts = await this.postRepo.find();
+    return {
+      user,
+      posts,
+    };
+  }
+
+  async findByUsername(username: string) {
+    return this.userRepo.findOne({
+      where: { username },
+    });
+  }
+}
